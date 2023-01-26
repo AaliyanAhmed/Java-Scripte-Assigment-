b392f8a115e615258b18e7d316061c5bf693e1bb
Aaliyan Ahmed <121717147+AaliyanAhmed@users.noreply.github.com>
2023-01-26 10:50:38 -0800

    Practice a Alert Chapter on Java Script

---
+++ Assigment 1/app.js
@@ -0,0 +1,9 @@
+//Q1,2 
+alert('Error! Pleas ENter a Valid a Password')
+//Q3
+alert('Welcon to JS land. . . \n Happy Codding')
+//Q4
+alert('Welcome to JS land. . .')
+alert('HappyCdding! \n Prevent this Page from Creating additional dialoge')
+//Q5
+alert("Hello. . .I can run JSthrough my web browser's console")
